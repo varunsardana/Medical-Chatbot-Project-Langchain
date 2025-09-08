ac4bd38009f41057fe483b77f2cd1e525bd96f5e
varunsardana <varunsardana2006@gmail.com>
2025-09-07 23:01:54 -0700

    project structure initiated

---
+++ template.sh
@@ -0,0 +1,17 @@
+# Creating directories
+
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo "Directory and file structure created successfully."
