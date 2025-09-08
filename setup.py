58375fd6521d1849fd9c79621fa9739db40fbe48
varunsardana <varunsardana2006@gmail.com>
2025-09-08 00:34:33 -0700

    set up done

---
+++ setup.py
@@ -0,0 +1,13 @@
+# We write setup.py as this makes your project installable as a python package
+
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author = "Varun Sardana",
+    author_email = "varunsardana2006@gmail.com",
+    packages = find_packages(),
+    install_requires= []
+
+)
